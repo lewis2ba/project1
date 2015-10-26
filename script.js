8452455d63498e82c9add7a067fd8f526eab32c7
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2015-10-26 11:42:58 -0400

    finished reset button, match and click counters

--- script.js
+++ script.js
@@ -1,46 +1,71 @@
 $(document).ready(function(){
   var cardNums = []
   var matches=0;
+  numOfClicks = 0;
   var clickCounter=0;
+  click = true
   //console.log(clickCounter)
 $('td').on("click",chooseCard)
+$('button').on("click",resetBoard)
 
   function chooseCard(){
+    numOfClicks++;
+    $('#clicks').text(numOfClicks)
+    //console.log(numOfClicks)
     //console.log(clickCounter)
     clickCounter=clickCounter+1
     var cardNum=$(this).text()
     cardNums.unshift(cardNum)
-    $(this).attr("class","content clicked")
+    $(this).attr("class","content clicked touched")
     //console.log(cardNums)
-    console.log(clickCounter)
+    //console.log(clickCounter)
     //console.log(clickCounter)
     if(clickCounter===2){
       var match = checkForMatch(cardNums[0],cardNums[1])
-      console.log(match)
+      //console.log(match)
       //$('td').removeAttr("class","content")
       clickCounter = 0
       cardNum = []
-
         if(match===true){
           $('.clicked').css('visibility','hidden')
+          console.log(matches)
+          if(matches===8){
+            winGame(numOfClicks)
+          }
         }
         else{
           $('td').removeAttr("class","content")
         }
+      }
     }
 
-  }
 
   function checkForMatch(num1,num2){
     if(num1===num2){
       matches+=1
-      alert("its a match!")
+      $("#matches").text(matches)
+      alert("It's a match!")
       //console.log(matches)
       return true;
-    }
+      }
+
     else{
-      alert("sorry! not a match, try again")
+      alert("Sorry! Not a match, try again!")
       return false;
     }
   }
 })
+
+  function winGame(numOfClicks){
+    alert("Congratulations, you win! You won the game in " + numOfClicks +" clicks." )
+    resetBoard()
+  }
+    function resetBoard(){
+      $('td').removeAttr("class","content")
+      $('td').css('visibility','visible')
+      numOfClicks = 0
+      $('#clicks').text(numOfClicks)
+      matches = 0
+      $("#matches").text(matches)
+  //randomizeBoard()
+}
